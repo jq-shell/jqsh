0926a5bfe7be8c8428a1ffe2f5f350c0d29d2180
Fenhl <fenhl@fenhl.net>
2015-01-07 07:21:01 +0000

    Simplify the main loop with `while let` and change the prompt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 extern crate readline;
 
 fn main() {
-    println!("press ctrl-c to exit");
-    loop {
-        match readline::readline(">>> ") {
-            Some(source) => {
-                readline::add_history(source.as_slice());
-//              let ast = parser::parse(source);
-//                println!("{}", ast.to_string());
-                println!("{}", source);
-            },
-            None => break
-        }
+    while let Some(source) = readline::readline("jqsh> ") {
+        readline::add_history(source.as_slice());
+        //let ast = parser::parse(source);
+        //println!("{}", ast.to_string());
+        println!("{}", source);
     }
 }
