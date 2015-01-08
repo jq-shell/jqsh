1dac30246e847c1d5d97e9cbba8d863087de99e3
Fenhl <fenhl@fenhl.net>
2015-01-08 09:13:11 +0000

    Use a drop-in replacement for readline until the readline crate is updated to the latest Rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-extern crate readline;
+//extern crate readline;
+
+mod readline { // temporary readline drop-in
+    use std::io;
+    
+    #[allow(unused_variables)]
+    pub fn add_history(hist_line: &str) {
+        // history? What history?
+    }
+    
+    pub fn readline(prompt: &str) -> Option<String> {
+        print!("{}", prompt);
+        match io::stdin().read_line().map(|line| line.as_slice().trim_right().to_string()) {
+            Err(_) => None,
+            Ok(line) => if line.as_slice() == "exit" { None } else { Some(line) }
+        }
+    }
+}
 
 fn main() {
     while let Some(source) = readline::readline("jqsh> ") {
